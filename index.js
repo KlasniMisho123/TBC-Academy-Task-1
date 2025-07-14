4a4e4bfb04169d9681e21d6aab7cf65c030d439e
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-14 22:00:43 +0400

    task 2

--- index.js
+++ index.js
@@ -8,10 +8,37 @@ class VirtualLibrary {
     }
 
     addBook(book) {
-        this.books.push({...this.books, available: true, borrowCount: 0})
+        this.books.push({ ...book, available: true, borrowCount: 0});
     }
+
+    removeBook(bookId) {
+        const book = this.books.find(b => b.id === bookId);
+        if (!book) {
+            return console.log("Book doesn't Exists")
+        } else if (!book.available) {
+            return console.log("Can't remove currently borrowed book")
+        } else {
+            this.books = this.books.filter(b => b.id !== bookId);
+            console.log(`Book named ${book.title} was removed`)
+        }
+    }
+
 }
 
 const db = new VirtualLibrary
 
-console.log(db)
+dummyBookData = [
+    { id: 1, title: "JS Mastery", author: "Kyle", genre: "Tech", rating: 4.8, year: 2020 },
+    { id: 2, title: "History of Europe", author: "Smith", genre: "History", rating: 4.1, year: 2008 },
+    { id: 3, title: "Romance of Code", author: "Jane", genre: "Romance", rating: 4.7, year: 2019 },
+    { id: 4, title: "Fantasy World", author: "Martin", genre: "Fantasy", rating: 4.9, year: 2022 },
+    { id: 5, title: "Learn Fast", author: "Amy", genre: "Tech", rating: 4.2, year: 2021 },
+]
+
+dummyBookData.forEach(book => {
+    db.addBook(book)
+});
+
+db.removeBook(2);
+
+console.log(db.books)
