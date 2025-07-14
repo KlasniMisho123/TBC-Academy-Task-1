6afef0ee62a3c1192d70b1c469d0a1fe124a9ee9
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-07-14 22:23:57 +0400

    task2 borrow book commit

--- index.js
+++ index.js
@@ -23,6 +23,30 @@ class VirtualLibrary {
         }
     }
 
+    borrowBook(userName, bookId) {
+        const book = this.books.find(b => b.id === bookId);
+        if(!book) {
+            return console.log("We dont have that book")
+        } 
+        if (!book.available) {
+            return console.log("Book is not available")
+        } 
+
+        let user = this.users.find(u => u.name === userName)
+        if(!user) {
+            user = {name: userName, borrowBooks: [], penaltyPoints: 0};
+            this.users.push(user)
+        }
+
+        const borrowDate = new Date();
+        const dueDate = new Date(borrowDate);
+        dueDate.setDate(borrowDate.getDate() + 14);
+
+        user.borrowed.push({ bookId, borrowDate, dueDate });
+        book.available = false;
+        book.borrowCount++;
+        console.log(`User: ${userName} borrowed book:${book.title}`)
+    }
 }
 
 const db = new VirtualLibrary
@@ -39,6 +63,9 @@ dummyBookData.forEach(book => {
     db.addBook(book)
 });
 
+db.borrowBook("misho", 1)
+
 db.removeBook(2);
 
+console.log(db.users)
 console.log(db.books)
